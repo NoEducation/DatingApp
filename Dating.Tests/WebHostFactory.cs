1f43217140d80f06229fac2d9f01222e65b4636b
Dominik <dominik.atrasik@primaris.eu>
2020-04-11 23:27:50 +0200

    moj commit

---
+++ Dating.Tests/WebHostFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dating.Tests
+{
+    public class WebHostFactory<TStartup>
+        : WebApplicationFactory<TStartup> where TStartup : class
+    {
+    }
+}
