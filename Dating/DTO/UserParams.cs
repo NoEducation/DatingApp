d5c2559cb46c9a399b9312fc0e44463c4f7df218
Dominik <dominik.atrasik@primaris.eu>
2020-05-08 21:57:19 +0200

    Added backend basic pagination

---
+++ Dating/DTO/UserParams.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dating.DTO
+{
+    public class UserParams
+    {
+        private const int MaxPageSize = 50;
+        public int PageNumber { get; set; }
+
+
+        private int _pageSize;
+        public int PageSize
+        {
+            get { return _pageSize;} set { _pageSize = value > MaxPageSize ? MaxPageSize : value}; }
+    }
+}
